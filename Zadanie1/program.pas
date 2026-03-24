321aa322522ce7302a0ab4c449af70fa65666d31
Kacper <kacper_chlastawa@epam.com>
2026-03-24 20:39:39 +0100

    Task 1: Implement BubbleSort procedure

--- Zadanie1/program.pas
+++ Zadanie1/program.pas
@@ -5,7 +5,6 @@ uses sysutils;
 type
     TIntArray = array of Integer;
 
-{ 3.0: Procedure to generate 50 random numbers from 0 to 100 }
 procedure GenerateNumbers(var Arr: TIntArray);
 var
     i: Integer;
@@ -13,18 +12,41 @@ begin
     SetLength(Arr, 50);
     Randomize;
     for i := 0 to 49 do
-        Arr[i] := Random(101); { Random from 0 to 100 }
+        Arr[i] := Random(101);
+end;
+
+procedure BubbleSort(var Arr: TIntArray);
+var
+    i, j, temp, n: Integer;
+begin
+    n := Length(Arr);
+    for i := 0 to n - 2 do
+        for j := 0 to n - i - 2 do
+            if Arr[j] > Arr[j + 1] then
+            begin
+                temp := Arr[j];
+                Arr[j] := Arr[j + 1];
+                Arr[j + 1] := temp;
+            end;
 end;
 
 var
     MyArray: TIntArray;
     i: Integer;
 begin
-    Writeln('Generating 50 random numbers:');
+    Writeln('Generating 50 random numbers...');
     GenerateNumbers(MyArray);
     
+    Writeln('Before sorting:');
+    for i := 0 to High(MyArray) do Write(MyArray[i], ' ');
+    Writeln;
+    Writeln;
+
+    Writeln('Sorting using Bubble Sort...');
+    BubbleSort(MyArray);
+    
+    Writeln('Sorted result:');
     for i := 0 to High(MyArray) do
         Write(MyArray[i], ' ');
-    
     Writeln;
 end.
